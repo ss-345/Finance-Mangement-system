1747ad29c083f3647e15904e4c6570a280b0749c
ss-345 <153381063+ss-345@users.noreply.github.com>
2026-04-03 17:32:08 +0530

    Update baseURL to include HTTPS

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "finance-mangement-system-backend.vercel.app/api",
+  baseURL: "https://finance-mangement-system-backend.vercel.app/api",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
